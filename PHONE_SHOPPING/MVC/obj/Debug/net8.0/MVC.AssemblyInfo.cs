19a9616398d06c8aaf9f9523f12ba594a1bbd798
Duc8101 <vegitobasicinstict1810@gmail.com>
2024-06-07 11:20:44 +0700

    .

--- PHONE_SHOPPING/MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ PHONE_SHOPPING/MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27b33c186a8cb95ac6329e8564e72bc835b0295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64bb48f0410050ae3c1ac758d9e7352cc3e19af")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
